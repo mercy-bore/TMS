20bee495b9c2ed4e53a7985cace86f2a661db7ce
Mercy Bore <mercycherotich757@gmail.com>
2022-09-29 23:18:30 +0300

    inquiry, inquiry sent module added

--- src/main/java/com/TMS/project/Servlet.java
+++ src/main/java/com/TMS/project/Servlet.java
@@ -17,6 +17,8 @@ public class Servlet extends HttpServlet{
             wr.print(this.signup(null));
         else if (action != null && action.equalsIgnoreCase("login"))
             wr.print(this.login(null));
+        else if(action != null && action.equalsIgnoreCase("inquiry"))
+            wr.print(this.inquiry(null));
         else
             wr.print(this.home());
     }
@@ -30,12 +32,17 @@ public class Servlet extends HttpServlet{
         String fname = req.getParameter("fname");
         String lname = req.getParameter("lname");
         String phone = req.getParameter("phone");
+        String location = req.getParameter("location");
         String password = req.getParameter("password");
         String confirmPassword = req.getParameter("confirmPassword");
 
 
         boolean signup = action != null && action.equalsIgnoreCase("signup");
         boolean login = action != null && action.equalsIgnoreCase("login");
+        boolean inquiry = action != null && action.equalsIgnoreCase("inquiry");
+        boolean inquirySent = action != null && action.equalsIgnoreCase("inquirySent");
+
+
 
         String actionError = "";
         if (signup) {
@@ -58,6 +65,8 @@ public class Servlet extends HttpServlet{
             if (password != null && confirmPassword != null && !password.equals(confirmPassword))
                 actionError += "Password & confirm password do not match<br/>";
 
+
+
             if (actionError.equals(""))
                 wr.print(this.home());
             else
@@ -77,6 +86,19 @@ public class Servlet extends HttpServlet{
                 wr.print(this.loggedIn());
             else
                 wr.print(this.login(actionError));
+
+        }else if(inquiry){
+            System.out.println("Location: " + location);
+            if (location == null || location.equalsIgnoreCase(""))
+                actionError += "Your Location is required.<br/>";
+
+            if (actionError.equals(""))
+                wr.print(this.inquirySent());
+            else
+                wr.print(this.inquiry(actionError));
+
+        } else if (inquirySent) {
+            System.out.println("Inquiry received.");
 
         }
     }
@@ -191,7 +213,128 @@ public class Servlet extends HttpServlet{
                 + "<input type=\"password\" name=\"confirmPassword\"  placeholder=\"Confirm Password\">"
                 +"</div>"
                 +"<div class=\"field space\">"
-                + "<tr> <td> <input type=\"submit\" value=\"Submit\"></tr> "
+                + "<tr> <td> <input type=\"submit\" value=\"Signup\"></tr> "
+                +"</div>"
+                + "</form>"
+                + "<span style=\"color:yellow\">" + (actionError != null? actionError : "") + "</span>"
+                +"</div>"
+                +"</div>"
+                + "</body>"
+                + "</html>";
+    }
+public String inquiry(String actionError){
+        return "<!DOCTYPE html>"
+                + "<html> "
+                + "<head> "
+                + "<title>TMS - Inquiry Page</title>"
+                + "</head>"+
+                "<style>"+
+                "*{color:#ececec; background-color: #142d4c}"+
+                "h2{text-align:center}"+
+                "h5{text-align:center}"+
+                "import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Poppins:400,500&display=swap');"+
+                "*{margin: 0;padding: 0;box-sizing: border-box;user-select: none;}"+
+                ".bg-img{background: url('bg.jpg');height: 100vh;background-size: cover;background-position: center;}"+
+                ".bg-img:after{position: absolute;content: '';top: 0;left: 0;height: 100%;width: 100%;background-color: #142d4c;}"+
+                ".content{position: absolute;top: 50%;left: 50%; z-index: 999; text-align: center; padding: 60px 32px;width: 370px;transform: translate(-50%,-50%); background: rgba(255,255,255,0.04);box-shadow: -1px 4px 28px 0px rgba(0,0,0,0.75);}"+
+                "tt,.content header{color: white;font-size: 33px;font-weight: 600;margin: 0 0 35px 0;font-family: 'Montserrat',sans-serif;}"+
+                ".field{position: relative;height: 45px;width: 100%;display: flex;background: rgba(255,255,255,0.94);}"+
+                ".field span{ color: #222;width: 40px;line-height: 45px;}"+
+                ".field input{height: 100%; width: 100%;background: transparent; border: none;outline: none; color: #222;font-size: 16px; font-family: 'Poppins',sans-serif;}"+
+                ".space{margin-top: 16px;}"+
+                ".show{position: absolute;right: 13px;font-size: 13px;font-weight: 700;color: #222;display: none;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                ".pass-key:valid ~ .show{display: block;}"+
+                ".pass{text-align: left;margin: 10px 0;}"+
+                ".pass a{color: white;text-decoration: none;font-family: 'Poppins',sans-serif;}"+
+                ".pass:hover a{text-decoration: underline;}"+
+                ".field input[type=\"submit\"]{background: #3498db;border: 1px solid #2691d9;color: white;font-size: 18px;letter-spacing: 1px;font-weight: 600;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                ".field input[type=\"submit\"]:hover{background: #2691d9;}"+
+                ".login{color: white;margin: 20px 0;font-family: 'Poppins',sans-serif;}"+
+                ".links{display: flex;cursor: pointer;color: white;margin: 0 0 20px 0;}"+
+                ".links i{font-size: 17px;}"+
+                "i span{margin-left: 8px;font-weight: 500;letter-spacing: 1px;font-size: 16px;font-family: 'Poppins',sans-serif;}"+
+                "</style>"
+                + "<body>"
+                +"<div class=\"bg-img\">"
+                +"<div class=\"content\">"
+                +"<header>Transport Management System</header>"
+                +"<header>Inquiry Form </header>"
+                + "<form action=\"./project\" method=\"post\">"
+                +"<div class=\"field\">"
+                +"<input type=\"text\" name=\"action\" value=\"inquiry\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<input type=\"text\" name=\"fname\" required placeholder=\"First Name\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<input type=\"text\" name=\"lname\" required placeholder=\"Last Name\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<input type=\"text\" name=\"phone\" required placeholder=\"Phone No\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<input type=\"text\" name=\"location\" placeholder=\"Location\">"
+                +"</div>"
+
+                +"<div class=\"field space\">"
+                + "<tr> <td> <input type=\"submit\" value=\"Send \"></tr> "
+                +"</div>"
+                + "</form>"
+                + "<span style=\"color:yellow\">" + (actionError != null? actionError : "") + "</span>"
+                +"</div>"
+                +"</div>"
+                + "</body>"
+                + "</html>";
+}
+    public String login(String actionError){
+        return  "<!DOCTYPE html>"
+                + "<html> "
+                + "<head> "
+                + "<title>TMS - Login Page</title>"
+                + "</head>"+
+                "<style>"+
+                "*{color:#ececec; background-color: #142d4c}"+
+                "h2{text-align:center}"+
+                "h5{text-align:center}"+
+                "import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Poppins:400,500&display=swap');"+
+                "*{margin: 0;padding: 0;box-sizing: border-box;user-select: none;}"+
+                ".bg-img{background: url('bg.jpg');height: 100vh;background-size: cover;background-position: center;}"+
+                ".bg-img:after{position: absolute;content: '';top: 0;left: 0;height: 100%;width: 100%;background-color: #142d4c;}"+
+                ".content{position: absolute;top: 50%;left: 50%; z-index: 999; text-align: center; padding: 60px 32px;width: 370px;transform: translate(-50%,-50%); background: rgba(255,255,255,0.04);box-shadow: -1px 4px 28px 0px rgba(0,0,0,0.75);}"+
+                "tt,.content header{color: white;font-size: 33px;font-weight: 600;margin: 0 0 35px 0;font-family: 'Montserrat',sans-serif;}"+
+                ".field{position: relative;height: 45px;width: 100%;display: flex;background: rgba(255,255,255,0.94);}"+
+                ".field span{ color: #222;width: 40px;line-height: 45px;}"+
+                ".field input{height: 100%; width: 100%;background: transparent; border: none;outline: none; color: #222;font-size: 16px; font-family: 'Poppins',sans-serif;}"+
+                ".space{margin-top: 16px;}"+
+                ".show{position: absolute;right: 13px;font-size: 13px;font-weight: 700;color: #222;display: none;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                ".pass-key:valid ~ .show{display: block;}"+
+                ".pass{text-align: left;margin: 10px 0;}"+
+                ".pass a{color: white;text-decoration: none;font-family: 'Poppins',sans-serif;}"+
+                ".pass:hover a{text-decoration: underline;}"+
+                ".field input[type=\"submit\"]{background: #3498db;border: 1px solid #2691d9;color: white;font-size: 18px;letter-spacing: 1px;font-weight: 600;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                ".field input[type=\"submit\"]:hover{background: #2691d9;}"+
+                ".login{color: white;margin: 20px 0;font-family: 'Poppins',sans-serif;}"+
+                ".links{display: flex;cursor: pointer;color: white;margin: 0 0 20px 0;}"+
+                ".links i{font-size: 17px;}"+
+                "i span{margin-left: 8px;font-weight: 500;letter-spacing: 1px;font-size: 16px;font-family: 'Poppins',sans-serif;}"+
+                "</style>"
+                + "<body>"
+                +"<div class=\"bg-img\">"
+                +"<div class=\"content\">"
+                +"<header>Transport Management System</header>"
+                +"<header>Login Form </header>"
+                + "<form action=\"./project\" method=\"post\">"
+                +"<div class=\"field\">"
+                + "<input type=\"text\" name=\"action\" value=\"login\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<input type=\"text\" name=\"username\" placeholder=\"Username/Email\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<input type=\"Password\" name=\"password\" placeholder=\"Password\">"
+                +"</div>"
+                +"<div class=\"field space\">"
+                + "<tr> <td> <input type=\"submit\" value=\"Login\"></tr> "
                 +"</div>"
                 + "</form>"
                 + "<span style=\"color:yellow\">" + (actionError != null? actionError : "") + "</span>"
@@ -201,35 +344,91 @@ public class Servlet extends HttpServlet{
                 + "</html>";
     }
 
-    public String login(String actionError){
-        return "<!DOCTYPE html>"
-                + "<html> "
-                + "<head> "
-                + "<h1> Login</h1>"
-                + "</head>"
-                + "<body>"
-                + "<form action=\"./project\" method=\"post\">"
-                + "<table> "
-                + "<tr> <td> Action: </td> <td> <input type=\"text\" name=\"action\" value=\"login\"> </td> </tr> "
-                + "<tr> <td> Email/Username: </td> <td> <input type=\"text\" name=\"username\"> </td> </tr> "
-                + "<tr> <td> Password: </td> <td> <input type=\"Password\" name=\"password\"> </td> </tr> "
-                + "<tr> <td> <input type=\"submit\" value=\"Submit\"></tr> "
-                + "</table>"
-                + "</form>"
-                + "<span style=\"color:red\">" + (actionError != null? actionError : "") + "</span>"
-                + "</body>"
-                + "</html>";
-    }
-
     public String loggedIn(){
-        return "<!DOCTYPE html>"
-                + "<html> "
-                + "<head> "
-                + "<h1> Login</h1>"
-                + "</head>"
-                + "<body>"
-                + "<span style=\"color:green;font-size: 24px;font-weight:bold\">Logged In</span>"
-                + "</body>"
-                + "</html>";
+        return
+                "<html>" +
+                        "<head>"+
+                        "<title>TMS - Homepage</title>" +
+                        "<link rel=\"stylesheet\" href=\"webapp/WEB-INF/assets/CSS/style.css\">"+
+                        "</head>" +
+                        "<style>"+
+                        "*{color:#ececec; background-color: #142d4c}"+
+                        "h2{text-align:center}"+
+                        "h5{text-align:center}"+
+                        "import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Poppins:400,500&display=swap');"+
+                        "*{margin: 0;padding: 0;box-sizing: border-box;user-select: none;}"+
+                        ".bg-img{background: url('bg.jpg');height: 100vh;background-size: cover;background-position: center;}"+
+                        ".bg-img:after{position: absolute;content: '';top: 0;left: 0;height: 100%;width: 100%;background-color: #142d4c;}"+
+                        ".content{position: absolute;top: 50%;left: 50%; z-index: 999; text-align: center; padding: 60px 32px;width: 370px;transform: translate(-50%,-50%); background: rgba(255,255,255,0.04);box-shadow: -1px 4px 28px 0px rgba(0,0,0,0.75);}"+
+                        "tt,.content header{color: white;font-size: 33px;font-weight: 600;margin: 0 0 35px 0;font-family: 'Montserrat',sans-serif;}"+
+                        ".field{position: relative;height: 45px;width: 100%;display: flex;background: rgba(255,255,255,0.94);}"+
+                        ".field span{ color: #222;width: 40px;line-height: 45px;}"+
+                        ".field input{height: 100%; width: 100%;background: transparent; border: none;outline: none; color: #222;font-size: 16px; font-family: 'Poppins',sans-serif;}"+
+                        ".space{margin-top: 16px;}"+
+                        ".show{position: absolute;right: 13px;font-size: 13px;font-weight: 700;color: #222;display: none;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                        ".pass-key:valid ~ .show{display: block;}"+
+                        ".pass{text-align: left;margin: 10px 0;}"+
+                        ".pass a{color: white;text-decoration: none;font-family: 'Poppins',sans-serif;}"+
+                        ".pass:hover a{text-decoration: underline;}"+
+                        ".field input[type=\"submit\"]{background: #3498db;border: 1px solid #2691d9;color: white;font-size: 18px;letter-spacing: 1px;font-weight: 600;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                        ".field input[type=\"submit\"]:hover{background: #2691d9;}"+
+                        ".login{color: white;margin: 20px 0;font-family: 'Poppins',sans-serif;}"+
+                        ".links{display: flex;cursor: pointer;color: white;margin: 0 0 20px 0;}"+
+                        ".links i{font-size: 17px;}"+
+                        "i span{margin-left: 8px;font-weight: 500;letter-spacing: 1px;font-size: 16px;font-family: 'Poppins',sans-serif;}"+
+                        "</style>"+
+                        "<body>"+
+                        "<div class=\"content\">"+
+                        "<h1>Welcome To Transport Management System.</h1>"+
+                        "<h3>Moving with you.</h3>"+
+                        "<h3>You have successfully logged in.</h3>"+
+                        "Would you like to make an inquiry? Send a queue below."+
+                        "<a href='./project?action=inquiry' style=\"text-decoration: none;\">  Make an inquiry  </a><br/>"+
+                        "</div>"+
+                        "</body>"+
+                        "</html>";
+
+    }
+    public String inquirySent(){
+        return
+        "<html>" +
+                "<head>"+
+                "<title>TMS - Homepage</title>" +
+                "<link rel=\"stylesheet\" href=\"webapp/WEB-INF/assets/CSS/style.css\">"+
+                "</head>" +
+                "<style>"+
+                "*{color:#ececec; background-color: #142d4c}"+
+                "h2{text-align:center}"+
+                "h5{text-align:center}"+
+                "import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700|Poppins:400,500&display=swap');"+
+                "*{margin: 0;padding: 0;box-sizing: border-box;user-select: none;}"+
+                ".bg-img{background: url('bg.jpg');height: 100vh;background-size: cover;background-position: center;}"+
+                ".bg-img:after{position: absolute;content: '';top: 0;left: 0;height: 100%;width: 100%;background-color: #142d4c;}"+
+                ".content{position: absolute;top: 50%;left: 50%; z-index: 999; text-align: center; padding: 60px 32px;width: 370px;transform: translate(-50%,-50%); background: rgba(255,255,255,0.04);box-shadow: -1px 4px 28px 0px rgba(0,0,0,0.75);}"+
+                "h2,h1,.content header{color: white;font-size: 33px;font-weight: 600;margin: 0 0 35px 0;font-family: 'Montserrat',sans-serif;}"+
+                ".field{position: relative;height: 45px;width: 100%;display: flex;background: rgba(255,255,255,0.94);}"+
+                ".field span{ color: #222;width: 40px;line-height: 45px;}"+
+                ".field input{height: 100%; width: 100%;background: transparent; border: none;outline: none; color: #222;font-size: 16px; font-family: 'Poppins',sans-serif;}"+
+                ".space{margin-top: 16px;}"+
+                ".show{position: absolute;right: 13px;font-size: 13px;font-weight: 700;color: #222;display: none;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                ".pass-key:valid ~ .show{display: block;}"+
+                ".pass{text-align: left;margin: 10px 0;}"+
+                ".pass a{color: white;text-decoration: none;font-family: 'Poppins',sans-serif;}"+
+                ".pass:hover a{text-decoration: underline;}"+
+                ".field input[type=\"submit\"]{background: #3498db;border: 1px solid #2691d9;color: white;font-size: 18px;letter-spacing: 1px;font-weight: 600;cursor: pointer;font-family: 'Montserrat',sans-serif;}"+
+                ".field input[type=\"submit\"]:hover{background: #2691d9;}"+
+                ".login{color: white;margin: 20px 0;font-family: 'Poppins',sans-serif;}"+
+                ".links{display: flex;cursor: pointer;color: white;margin: 0 0 20px 0;}"+
+                ".links i{font-size: 17px;}"+
+                "i span{margin-left: 8px;font-weight: 500;letter-spacing: 1px;font-size: 16px;font-family: 'Poppins',sans-serif;}"+
+                "</style>"+
+                "<body>"+
+                "<div class=\"content\">"+
+                "<h1> Transport Management System.</h1>"+
+                "<h2>Your Inquiry  has been successfully sent.</h2>"+
+                "</div>"+
+                "</body>"+
+                "</html>";
     }
 }
+
